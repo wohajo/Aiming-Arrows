0132851bcfe8e631c8ebcb8a261ee1b793e08ef5
Michał Prawda <wohajo@pop-os.localdomain>
2019-12-27 15:55:24 +0100

    added basic gui along with new classes, packages and resources

---
+++ celowanie/src/gui/MainGUI.java
@@ -0,0 +1,72 @@
+package gui;
+
+import gameLogic.GameStart;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import javafx.scene.layout.BorderPane;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class MainGUI {
+
+    public MainGUI(Stage stage) throws FileNotFoundException {
+        stage.setTitle("Aiming Arrows");
+
+        Label topLabel = new Label("AIMING ARROWS");
+        topLabel.setPadding(new Insets(20, 0, 20, 0));
+        topLabel.setFont(Font.font("Cambria", 32));
+
+        HBox topPane = new HBox();
+        topPane.getChildren().add(topLabel);
+        topPane.setAlignment(Pos.CENTER);
+        topPane.setBackground(new Background(new BackgroundFill(Color.rgb(10, 10, 20), CornerRadii.EMPTY, Insets.EMPTY)));
+
+        ImageView saveGameImg = new ImageView(new Image(new FileInputStream("rsc/saveFile.png")));
+        Button saveFileButton = new Button("Zapisz", saveGameImg);
+        saveFileButton.setPrefWidth(100);
+        saveFileButton.setOnAction(value ->  {
+            topLabel.setText("Save game");
+        });
+
+        ImageView loadFileImg = new ImageView(new Image(new FileInputStream("rsc/loadFile.png")));
+        Button openFileButton = new Button("Wczytaj", loadFileImg);
+        openFileButton.setPrefWidth(100);
+        openFileButton.setOnAction(value ->  {
+            topLabel.setText("Load game");
+        });
+
+        ImageView exitGameImg = new ImageView(new Image(new FileInputStream("rsc/exitGame.png")));
+        Button exitGameButton = new Button("Wyjście", exitGameImg);
+        exitGameButton.setPrefWidth(100);
+        exitGameButton.setOnAction(value ->  {
+            topLabel.setText("Exit game");
+        });
+
+        VBox rightPane = new VBox();
+        rightPane.getChildren().addAll(openFileButton, saveFileButton, exitGameButton);
+        rightPane.setSpacing(10);
+        rightPane.setPadding(new Insets(0, 20, 0, 20));
+        rightPane.setAlignment(Pos.TOP_CENTER);
+        rightPane.setBackground(new Background(new BackgroundFill(Color.rgb(10, 10, 20), CornerRadii.EMPTY, Insets.EMPTY)));
+
+        BorderPane mainWindow = new BorderPane();
+        mainWindow.setRight(rightPane);
+        mainWindow.setTop(topPane);
+        mainWindow.setCenter(null);
+        mainWindow.setAlignment(topPane, Pos.TOP_CENTER);
+
+        Scene scene = new Scene(mainWindow, 800, 600);
+        stage.setScene(scene);
+
+        stage.show();
+    }
+
+}
--- celowanie/src/mainApp/Main.java
+++ celowanie/src/mainApp/Main.java
@@ -1,62 +1,18 @@
 package mainApp;
 
+import gui.MainGUI;
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.canvas.*;
-import javafx.scene.web.*;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.shape.*;
-import javafx.stage.StageStyle;
-
 
 public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
         try {
-            stage.setTitle("Aiming Arrows");
-
-            Label topLabel = new Label("AIMING ARROWS");
-            topLabel.setPadding(new Insets(10, 0, 0, 20));
-            topLabel.setFont(Font.font("Cambria", 32));
-            //topLabel.setBackground(new Background());
-
-            Button openFile = new Button("Wczytaj");
-            openFile.setPrefWidth(100);
-            Button saveFile = new Button("Zapisz");
-            saveFile.setPrefWidth(100);
-            Button exitGame = new Button("Zakończ grę");
-            exitGame.setPrefWidth(100);
-            VBox rightPane = new VBox();
-            rightPane.getChildren().addAll(openFile, saveFile, exitGame);
-            rightPane.setSpacing(10);
-            rightPane.setPadding(new Insets(0, 20, 0, 0));
-
-            rightPane.setAlignment(Pos.TOP_CENTER);
-
-            //creating main window and hooking up nodes inside
-            BorderPane mainWindow = new BorderPane();
-            mainWindow.setRight(rightPane);
-            mainWindow.setTop(topLabel);
-            mainWindow.setAlignment(topLabel, Pos.TOP_CENTER);
-
-            Scene scene = new Scene(mainWindow, 800, 600);
-            stage.setScene(scene);
-
-            stage.show();
+            MainGUI game = new MainGUI(stage);
         }
 
         catch (Exception e) {
-
             System.out.println(e.getMessage());
         }
     }
