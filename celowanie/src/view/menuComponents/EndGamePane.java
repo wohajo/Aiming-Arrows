1e243f2ce74edc6a6033ee4288568a48d50e813e
wohajo <michalos222@gmail.com>
2020-01-08 13:42:44 +0100

    added winning game pane

---
+++ celowanie/src/view/menuComponents/EndGamePane.java
@@ -0,0 +1,20 @@
+package view.menuComponents;
+
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+
+public class EndGamePane extends HBox {
+    private Text welcomeText;
+
+    public EndGamePane() {
+        this.welcomeText = new Text("Welcome to Aiming Arrows! Click help to get \ninformations about the game and try out default board.\n" +
+                "Click 'start' to start the game with default \nboard or 'load' to load a new board.");
+        this.getChildren().add(welcomeText);
+        this.setId("description");
+    }
+
+    public EndGamePane getEndGamePane() {
+        return this;
+    }
+}
+}
